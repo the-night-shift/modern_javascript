adfb1ba8c2339d631dd2ec082063f5ae960fa768
Brian <riceb53@gmail.com>
2018-04-23 21:25:44 -0500

    add a bunch of new npm packages

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+var moment = require('moment');
+var math = require('mathjs');
+
 console.log('index.js is working');
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+// use math.js
+math.sqrt(-4); // 2i
+console.log(math.sqrt(-4));
+console.log('hello');
